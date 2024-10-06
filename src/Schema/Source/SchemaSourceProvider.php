1cc04ed944ef35c46b48ca6b985c773491ee574d
connor <connorsmyth@rogers.com>
2024-10-06 17:31:54 -0400

    Back to the drawing board

--- src/Schema/Source/SchemaSourceProvider.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Lantern\Schema\Source;
-
-interface SchemaSourceProvider
-{
-    /** Provide the string contents of the schema definition. */
-    public function getSchemaString(): string;
-}
